6e0fcee79fd1dba2439ae7bcc22566e361ccf9bf
pseudosma <mrdzpaige@ghoulmail.net>
2023-08-24 01:47:51 -0500

    master/detail

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,22 +1,17 @@
 import 'package:ngdart/angular.dart';
 import 'package:ngforms/ngforms.dart';
-import 'hero.dart';
+import 'src/hero.dart';
+import 'src/mock_heros.dart';
 @Component(
   selector: 'my-app',
-  template: '''
-  <h1>{{title}}</h1>
-  <h2>{{hero.name}}</h2>
-  <div>
-    <label>id: </label>{{hero.id}}
-  </div>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="hero.name" placeholder="name">
-  </div>
-''',
-  directives: [formDirectives],
+  templateUrl: 'app_component.html',
+  directives: [coreDirectives, formDirectives],
+  styleUrls: ['app_component.css'],
 )
 class AppComponent {
   final title = 'Tour of Heroes';
-  Hero hero = Hero(1, 'Windstorm');
+  List<Hero> heroes = mockHeroes;
+  Hero? selected;
+
+  void onSelect(Hero hero) => selected = hero;
 }
