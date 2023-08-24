463228c43f81820a8b30c54ce8525e660431ee47
pseudosma <mrdzpaige@ghoulmail.net>
2023-08-24 18:31:01 -0500

    services

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,19 +1,30 @@
 import 'package:ngdart/angular.dart';
 import 'package:ngforms/ngforms.dart';
 import 'src/hero.dart';
-import 'src/mock_heros.dart';
+import 'src/mock_heroes.dart';
 import 'src/hero_component.dart';
+import 'src/hero_service.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
   directives: [coreDirectives, HeroComponent],
   styleUrls: ['app_component.css'],
+  providers: [ClassProvider(HeroService)],
 )
-class AppComponent {
+class AppComponent implements OnInit {
+  final HeroService _heroService;
   final title = 'Tour of Heroes';
   List<Hero> heroes = mockHeroes;
   Hero? selected;
 
+  AppComponent(this._heroService);
+
   void onSelect(Hero hero) => selected = hero;
+
+  Future<void> _getHeroes() async {
+    heroes = await _heroService.getAll();
+  }
+  
+  void ngOnInit() => _getHeroes();
 }
